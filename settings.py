aaf5dcacdf8dbe843b0dbf8951a27d05cc04e2ce
CoderSourojeet <sourojeetshyam@gmail.com>
2024-02-14 19:24:57 +0530

    settings update

--- settings.py
+++ settings.py
@@ -6,5 +6,14 @@ import math
 # window resolution
 WIN_RES = glm.vec2(800, 600)
 
+# camera
+ASPECT_RATIO = WIN_RES.x / WIN_RES.y
+FOV_DEG = 50
+V_FOV = glm.radians(FOV_DEG)
+H_FOV = 2 * math.atan(math.tan(V_FOV * 0.5) * ASPECT_RATIO)
+NEAR = 0.1
+FAR = 2000.0
+PITCH_MAX = glm.radians(89)
+
 # background colour
 BG_COLOR = glm.vec3(0.1, 0.16, 0.25)
